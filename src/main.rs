58e3b7892db09a0c6087dfb849f3cc1a67f38feb
Sven-Hendrik Haase <sh@lutzhaase.com>
2016-02-03 06:00:48 +0100

    Use handlebars instead of mustache

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 #[macro_use] extern crate nickel;
-extern crate nickel_mustache;
+extern crate handlebars;
 extern crate ws;
 
 use ws::{ connect, listen, CloseCode, Sender, Handler, Message, Result, Handshake };
 use nickel::{Nickel, HttpRouter};
-use nickel_mustache::{TemplateSupport, Render};
+use handlebars::Handlebars;
 
 use std::collections::HashMap;
 use std::thread;
+use std::io::prelude::*;
+use std::io;
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+
+fn load_template(name: &str) -> String {
+    let path = Path::new(name);
+    let mut file = File::open(path).unwrap();
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("Couldn't load template '{}': {}", name, Error::description(&why)),
+        Ok(_) => s
+    }
+}
 
 fn main() {
+    let mut handlebars = Handlebars::new();
+
+    let t = load_template("templates/index.hbs");
+    handlebars.register_template_string("index", t).ok().unwrap();
+
     let mut server = Nickel::new();
-
-    server.get("/bar", middleware!("This is the /bar handler"));
-    server.get("/user/:herp", middleware! { |req, res|
-        let mut data = HashMap::new();
-        data.insert("test", req.param("herp"));
-
-        return Render::render(res, "html/index.mustache", &data);
-    });
-
-    // server.listen("127.0.0.1:6767");
 
     struct WebsocketServer {
         out: Sender
@@ -49,6 +59,15 @@ fn main() {
             WebsocketServer { out: out }
         }).unwrap()
     });
+
+    server.get("/bar", middleware!("This is the /bar handler"));
+    server.get("/user/:herp", middleware! { |req, res|
+        let mut data = HashMap::new();
+        data.insert("test".to_string(), req.param("herp").unwrap().to_string());
+        handlebars.render("index", &data).ok().unwrap()
+    });
+
+    server.listen("127.0.0.1:6767");
 
     websocket_server.join();
 }
