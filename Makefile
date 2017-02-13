1bea306610cd3bfb3ac0f917d64a7cf665b6c07c
Sven-Hendrik Haase <svenstaro@gmail.com>
2017-02-14 00:06:01 +0100

    Convert this into a frontend project

---
+++ Makefile
@@ -0,0 +1,11 @@
+default: build
+
+.PHONY: build
+build:
+	cargo build --target=asmjs-unknown-emscripten
+	cargo build --target=wasm32-unknown-emscripten
+
+.PHONY: run
+run: build
+	cp target/asmjs-unknown-emscripten/debug/rust-web-experiments.* data/
+	cd data && python -m http.server
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,9 @@
-#[macro_use] extern crate nickel;
-extern crate handlebars;
-extern crate ws;
-
-use ws::{ listen, CloseCode, Sender, Handler, Message, Result, Handshake };
-use nickel::{Nickel, HttpRouter};
-use handlebars::Handlebars;
-
-use std::collections::HashMap;
-use std::thread;
-use std::io::prelude::*;
-use std::fs::File;
-use std::path::Path;
-use std::error::Error;
-
-struct WebsocketServer {
-    out: Sender
-}
-
-impl Handler for WebsocketServer {
-    fn on_message(&mut self, msg: Message) -> Result<()> {
-        println!("herp: {}", msg);
-        self.out.send(msg)
-    }
-
-    fn on_open(&mut self, handshake: Handshake) -> Result<()> {
-        println!("connection: {:?}", handshake);
-        self.out.send("sup")
-    }
-
-    fn on_close(&mut self, code: CloseCode, reason: &str) {
-        println!("Closing: ({:?}) {}", code, reason);
-        println!("Shutting down");
-        self.out.shutdown().unwrap()
-    }
-}
+extern crate webplatform;
 
 fn main() {
-    let mut handlebars = Handlebars::new();
-
-    handlebars.register_template_file("index", &Path::new("templates/index.hbs")).ok().unwrap();
-
-    let mut server = Nickel::new();
-
-    let websocket_server = thread::spawn(move || {
-        listen("127.0.0.1:3012", |out| {
-            WebsocketServer { out: out }
-        }).unwrap()
-    });
-
-    server.get("/bar", middleware!("This is the /bar handler"));
-    server.get("/user/:herp", middleware! { |req, res|
-        let mut data = HashMap::new();
-        data.insert("test".to_string(), req.param("herp").unwrap().to_string());
-        handlebars.render("index", &data).ok().unwrap()
-    });
-
-    server.listen("127.0.0.1:6767");
-
-    let _ = websocket_server.join();
+    let document = webplatform::init();
+    let body = document.element_query("body").unwrap();
+    body.html_set("<h1>HELLO FROM RUST</h1> <button>CLICK ME</button>");
+    let button = document.element_query("button").unwrap();
+    button.on("click", |_| webplatform::alert("WITNESS ME"));
 }
