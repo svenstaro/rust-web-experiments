69f19f07c8689ef17fc59e38fff91251985029bf
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-01-16 09:12:11 +0100

    Update yew

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ extern crate chrono;
 extern crate yew;
 
 use chrono::prelude::*;
-use yew::html::*;
+use yew::prelude::*;
+use yew::services::console::ConsoleService;
+
+struct Context {
+    console: ConsoleService,
+}
 
 struct Model {
     value: i64,
@@ -12,35 +17,61 @@ struct Model {
 enum Msg {
     Increment,
     Decrement,
+    Bulk(Vec<Msg>),
 }
 
-fn update(_: &mut Context<Msg>, model: &mut Model, msg: Msg) {
-    match msg {
-        Msg::Increment => {
-            model.value = model.value + 1;
+impl Component<Context> for Model {
+    type Msg = Msg;
+    type Properties = ();
+
+    fn create(_: &mut Env<Context, Self>) -> Self {
+        Model {
+            value: 0,
         }
-        Msg::Decrement => {
-            model.value = model.value - 1;
+    }
+
+    fn update(&mut self, msg: Self::Msg, context: &mut Env<Context, Self>) -> ShouldRender {
+        match msg {
+            Msg::Increment => {
+                self.value = self.value + 1;
+                context.console.log("plus one");
+            }
+            Msg::Decrement => {
+                self.value = self.value - 1;
+                context.console.log("minus one");
+            }
+            Msg::Bulk(list) => {
+                for msg in list {
+                    self.update(msg, context);
+                }
+            }
         }
+        true
     }
 }
 
-fn view(model: &Model) -> Html<Msg> {
-    html! {
-        <div>
-            <nav class="menu",>
-                <button onclick=|_| Msg::Increment,>{ "Increment" }</button>
-                <button onclick=|_| Msg::Decrement,>{ "Decrement" }</button>
-            </nav>
-            <p>{ model.value }</p>
-            <p>{ Local::now() }</p>
-        </div>
+impl Renderable<Context, Model> for Model {
+    fn view(&self) -> Html<Context, Self> {
+        html! {
+            <div>
+                <nav class="menu",>
+                    <button onclick=|_| Msg::Increment,>{ "Increment" }</button>
+                    <button onclick=|_| Msg::Decrement,>{ "Decrement" }</button>
+                    <button onclick=|_| Msg::Bulk(vec!(Msg::Increment, Msg::Increment)),>{ "Increment Twice" }</button>
+                </nav>
+                <p>{ self.value }</p>
+                <p>{ Local::now() }</p>
+            </div>
+        }
     }
 }
 
 fn main() {
-    let model = Model {
-        value: 0,
+    yew::initialize();
+    let context = Context {
+        console: ConsoleService,
     };
-    program(model, update, view);
+    let app: App<_, Model> = App::new(context);
+    app.mount_to_body();
+    yew::run_loop();
 }
